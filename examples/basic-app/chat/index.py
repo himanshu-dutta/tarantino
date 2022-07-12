609050fe48757a629be167087ef4e1df6a5d8e39
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2022-07-12 16:45:16 +0530

    modified `examples/basic-app` chat with WebsocketHandler

--- examples/basic-app/chat/index.py
+++ examples/basic-app/chat/index.py
@@ -1,19 +1,25 @@
 from tarantino import SubApp
-from tarantino.websocket import WSConnection
+from tarantino.concurrency import set_interval
+from tarantino.websocket.handlers import WebsocketHandler
 
 subapp = SubApp(prefix="/chat")
 
 
 @subapp.websocket("/{username:str}")
-async def chat_handler(conn: WSConnection, username: str):
-    await conn.accept()
+class ChatHandler(WebsocketHandler):
+    async def send_ping_msg(self):
+        print("[Server PING] sending message")
+        await self.send(text_data=f"[Server PING]: {self.params['username']}")
 
-    await conn.send(text_data=f"Client: {username}")
+    async def on_connect(self):
+        print("Connection opened by user: ", self.params["username"])
+        await self.accept()
+        self.ping_close = await set_interval(self.send_ping_msg, 2)
 
-    while True:
-        msg = await conn.receive()
-        if msg["type"] == "websocket.disconnect":
-            return
+    async def on_message(self, text_data: str = None, bytes_data: bytes = None):
+        print("Msg from WS: ", text_data)
+        await self.send(text_data=f"Echo: {text_data}")
 
-        print("Msg from ws: ", msg)
-        await conn.send(text_data=msg.get("text"))
+    async def on_disconnect(self, code: int):
+        print("Close code: ", code)
+        self.ping_close()
